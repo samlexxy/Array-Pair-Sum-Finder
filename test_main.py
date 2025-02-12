d09c9e350e9c719ab8a79634249e2877e5e2c638
Samuel Ibiloye <ibiloyes@gmail.com>
2025-02-12 13:32:10 +0000

    Update test_main.py

--- test_main.py
+++ test_main.py
@@ -6,6 +6,7 @@ class TestGetUniquePairs(unittest.TestCase):
         self.assertEqual(get_unique_pairs([]), [])
 
     def test_no_duplicate_sums(self):
+        # Tes with an empty array
         array = [1, 2, 3]
         self.assertEqual(get_unique_pairs(array), [])
 
@@ -23,10 +24,9 @@ class TestGetUniquePairs(unittest.TestCase):
         ]
         self.assertEqual(get_unique_pairs(array), expected_output)
 
-    def test_with_large_array(self):
-        # Test with a larger array to ensure performance and correctness
+    def test_with_a_smaller_array(self):
+        # Test with a smaller array to correctness
         array = [4, 23, 65, 67, 24, 12, 86]
-        { 4, 23, 65, 67, 24, 12, 86}
         expected_output = ["Pairs : ( 4, 86) ( 23, 67) have sum : 90"]
         self.assertEqual(get_unique_pairs(array), expected_output)
 
